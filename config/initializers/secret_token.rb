d472e3f72513ba89a2c83c3fb9ac9eef844b8424
Jose Sanchez <jparkers0012@gmail.com>
2015-04-14 14:52:35 -0400

    Create User model with Devise, List and Item models and all serializers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+TodoApi::Application.config.secret_token = 'f8f1c52c49f882c7c046968b1578b40650b3c0a0e6abb4929d53cc4d2c48ffd49d43e3aeb942647c037a5ea6128df943354c34fbdc17d36a8319fddecb0d2bae'
